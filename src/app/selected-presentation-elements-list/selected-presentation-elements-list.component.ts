d4228ac9c1487f8a7c64612004af5e423841a4bd
Jan Martin Reckel <jm.reckel@t-online.de>
2024-09-12 21:18:50 +0200

    Remove bootstrap, add second context menu

--- src/app/selected-presentation-elements-list/selected-presentation-elements-list.component.ts
+++ src/app/selected-presentation-elements-list/selected-presentation-elements-list.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { FileManagerServiceService } from '../file-manager-service.service';
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatIconModule } from '@angular/material/icon';
 
 /* For the Context Menu */
 import { CdkMenuModule } from '@angular/cdk/menu'; 
+import { SongContextMenuComponent } from '../song-context-menu/song-context-menu.component';
 
 @Component({
   selector: 'app-selected-presentation-elements-list',
@@ -11,6 +14,9 @@ import { CdkMenuModule } from '@angular/cdk/menu';
   imports: [
     MatListModule,
     CdkMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    SongContextMenuComponent,
   ],
   templateUrl: './selected-presentation-elements-list.component.html',
   styleUrl: './selected-presentation-elements-list.component.css'
--- src/app/song-context-menu/song-context-menu.component.ts
+++ src/app/song-context-menu/song-context-menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, ViewChild } from '@angular/core';
 import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu'; 
 import { CantaraSongFile } from '../cantara-datatypes';
+import { FileManagerServiceService } from '../file-manager-service.service';
 
 @Component({
   selector: 'app-song-context-menu',
@@ -14,16 +15,38 @@ import { CantaraSongFile } from '../cantara-datatypes';
 export class SongContextMenuComponent {
   @ViewChild(MatMenuTrigger) trigger!: MatMenuTrigger;
 
-  ngAfterViewInit() {
-  }
-
-  toggleMenu() {
-    this.trigger.openMenu();
-  }
+  fileManagerService: FileManagerServiceService;
   
   @Input()
   song: CantaraSongFile | undefined;
   
   @Input()
   isRemovable: boolean = false;
+
+  index: number | null = null;
+
+  constructor(fileManagerService: FileManagerServiceService) {
+    this.fileManagerService = fileManagerService;
+  }
+
+  ngAfterViewInit() {
+  }
+
+  toggleMenu() {
+    this.trigger.openMenu();
+  }
+
+  toggleMenuWidth(song: CantaraSongFile, isRemovable: boolean, index: number | null = null) {
+    this.song = song;
+    this.isRemovable = isRemovable;
+    this.index = index;
+    this.toggleMenu();
+  }
+
+  deleteEntryFromSelectionIndex() {
+    if (this.index) { 
+      this.fileManagerService.selectedSongs.splice(this.index, 1);
+    }
+  }
+
 }
